194549e7fb27b2df2c37ec419f84b6b3a6c6b03e
royoo1 <1291341489@qq.com>
2017-05-18 09:03:25 +0800

    User.java

---
+++ test/User.java
@@ -0,0 +1,69 @@
+package model;
+
+public class User {
+	String userid;
+	String username;
+	String nikename;
+	String sex;
+	String school;
+	String password;
+	String phonenumber;
+	int comments;
+	float commentstar;
+	int usercash;
+	public String getUserid() {
+		return userid;
+	}
+	public void setUserid(String userid) {
+		this.userid = userid;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getNikename() {
+		return nikename;
+	}
+	public void setNikename(String nikename) {
+		this.nikename = nikename;
+	}
+	public String getSex() {
+		return sex;
+	}
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+	public String getSchool() {
+		return school;
+	}
+	public void setSchool(String school) {
+		this.school = school;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getPhonenumber() {
+		return phonenumber;
+	}
+	public void setPhonenumber(String phonenumber) {
+		this.phonenumber = phonenumber;
+	}
+	public int getComments() {
+		return comments;
+	}
+	public void setComments(int comments) {
+		this.comments = comments;
+	}
+	public float getCommentstar() {
+		return commentstar;
+	}
+	public void setCommentstar(float commentstar) {
+		this.commentstar = commentstar;
+	}
+	
+}
